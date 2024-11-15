cf6fef46f342c19f15b1c5d991bb415cf84ad387
mazharul haque Leon <mazharhaqueaiub@gmail.com>
2024-11-15 14:10:05 +0600

    all

---
+++ for c++/primitivedata.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int number = 50;
+
+char className ='D';
+
+float PI =3.25f;
+
+bool isFinite = false;
+
+double time = 6.99;
+
+
+    cout <<number<<endl;
+    cout<<className <<endl;
+    cout<<PI<<endl;
+    cout<<isFinite<<endl;
+    cout<<time<<endl;
+
+    return 0;
+}
