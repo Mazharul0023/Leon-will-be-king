2a8567c51cc24699fb23c0ec054543f2ec692aac
mazharul haque Leon <mazharhaqueaiub@gmail.com>
2024-12-03 17:08:31 +0600

    day 5

--- for c++/type.cpp
+++ for c++/type.cpp
@@ -3,8 +3,14 @@ using namespace std;
 int main()
 {
 
-    char A;
+float PI=3.14;
 
+
+    cout<< (int) (PI)<<endl;
     cout << 'A' + 1 << endl;
+    cout<< ((float) 10/3)<<endl;
+    cout<<(char) 'A' + 1 << endl;
+    cout<< ((bool)3+2) <<endl; //cz bool ar man 0 chara baki shob kichu te 1 . (bool)3 mane ture mane 1 sathe 2 =3
+    cout<<(25.6 + 2 + 'A') <<endl; //double <int < char = 25.6 +2+65=92.6 karon shob gula double a convert hobe 
     return 0;
 }
---
+++ for c++/unary.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    /*int a=5;
+    a++;
+
+    cout<<a<<endl; 
+
+
+    int a= 10;
+    a+=5;
+    cout<<a; 
+
+//relational operators >, >=, <, <=, ==, !=
+
+     int a=5;
+     int b =10;
+
+     cout<<(a<b)<<endl;
+     cout<<(a<=b)<<endl;
+     cout<<(a>b)<<endl;
+     cout<<(a>=b)<<endl;
+     cout<<(a==b)<<endl;
+     cout<<(a!=b);         */
+
+
+     // logical operators AND && , or || , not !
+
+    return 0;
+}
