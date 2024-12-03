2c000d7b143f320874988d7c0377dea7e2f8b06c
mazharul haque Leon <mazharhaqueaiub@gmail.com>
2024-12-03 21:43:18 +0600

    complete

--- for c++/ifelse.cpp
+++ for c++/ifelse.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 
-{
+/*{
 int age =20;
 if(age>=20) {
 cout<<"adult"<<endl;
@@ -14,5 +14,47 @@ else {
 
 }
 
-return 0; 
+return 0;
+}
+
+{
+int age;
+
+cout<<"Enter your age: ";
+cin>>age;
+
+if (age>=20)
+{
+
+    cout<<"You are an adult now."<<endl;
+}
+
+else
+{
+
+    cout<<"You are a child now."<<endl;
+}
+
+return 0;
+
+
+}*/
+
+{
+
+    float marks;
+    cout << "Enter your marks:";
+    cin >> marks;
+
+    if (marks >= 33)
+    {
+        cout << "congrats you are passed." << endl;
+    }
+
+    else
+    {
+        cout << "Sorry, you are failed." << endl;
+    }
+
+    return 0;
 }
